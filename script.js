b4818132668d3dad7195d0e0eeb072477da14417
ManiKumar-kmkr <kusumamanikumar@gmail.com>
2026-05-14 17:51:53 +0530

    final portfolio

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => {
+
+    document.getElementById("loader").style.display = "none";
+
+});
+
+var typed = new Typed(".typing", {
+
+    strings: [
+        "Software Developer",
+        "Web Developer",
+        "CSE Student"
+    ],
+
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+});
